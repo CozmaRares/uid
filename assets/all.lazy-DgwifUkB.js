ee12f588a02191542100ee2f59d20f869bdf05d2
Cozma Rares <cozmarares11@gmail.com>
2024-12-04 09:14:28 +0200

    Updates

--- assets/all.lazy-DgwifUkB.js
+++
@@ -1,1 +0,0 @@
-import{b as o,j as e}from"./index-C_fUzQuZ.js";const l=o("/votes/all")({component:t});function t(){return e.jsx("div",{children:'Hello "/votes/all"!'})}export{l as Route};
---
+++ assets/all.lazy-ok2hWqk1.js
@@ -0,0 +1,1 @@
+import{b as o,j as e}from"./index-D1kMV_c5.js";const l=o("/votes/all")({component:t});function t(){return e.jsx("div",{children:'Hello "/votes/all"!'})}export{l as Route};
