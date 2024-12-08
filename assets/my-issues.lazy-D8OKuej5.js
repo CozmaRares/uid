6f558755537a159b891426b914f31ed2681a98a0
Cozma Rares <cozmarares11@gmail.com>
2024-12-08 20:18:59 +0200

    Updates

--- assets/my-issues.lazy-D8OKuej5.js
+++
@@ -1,1 +0,0 @@
-import{d as s,j as e}from"./index-CF_X6mMS.js";const i=s("/issues/my-issues")({component:o});function o(){return e.jsx("div",{children:'Hello "/issues/my-issues"!'})}export{i as Route};
