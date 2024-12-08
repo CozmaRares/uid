64058956f9ccd7a8b7bd8b408acb089294afcaae
Cozma Rares <cozmarares11@gmail.com>
2024-12-08 20:15:33 +0200

    Updates

--- assets/_meedingID.lazy-BX1-Esb5.js
+++
@@ -1,1 +0,0 @@
-import{a as e,j as t}from"./index-DEsjeKTu.js";const i=e("/meetings/$meedingID")({component:n});function n(){return t.jsx("div",{children:'Hello "/meetings/$meedingID"!'})}export{i as Route};
---
+++ assets/index.lazy-fTBp691n.js
@@ -0,0 +1,1 @@
+import{d as t,j as e}from"./index-CF_X6mMS.js";const n=t("/activities/")({component:o});function o(){return e.jsx("div",{children:'Hello "/activities/"!'})}export{n as Route};
